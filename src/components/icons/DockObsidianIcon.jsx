bcaeb1c801f24b5a700b3c11e489ebb96165fdf4
Thanawan <thanawan@Thanawans-MacBook-Air.local>
2026-08-15 17:13:00 +0700

    feat: change obisidian logo

--- src/components/icons/DockObsidianIcon.jsx
+++ src/components/icons/DockObsidianIcon.jsx
@@ -1,13 +1,7 @@
+import obsidianLogo from '../../assets/icons/obsidian-logo.svg'
+
 const DockObsidianIcon = ({ className }) => {
-  return (
-    <svg viewBox="0 0 44 44" className={className} aria-hidden="true">
-      <rect width="44" height="44" rx="11" fill="#2b2b31" />
-      <polygon points="22,8 32,17 22,22" fill="#a78bfa" />
-      <polygon points="12,17 22,8 22,22" fill="#7c3aed" />
-      <polygon points="12,17 22,22 22,36" fill="#6d28d9" />
-      <polygon points="32,17 22,22 22,36" fill="#7c3aed" />
-    </svg>
-  )
+  return <img src={obsidianLogo} alt="" className={className} />
 }
 
 export default DockObsidianIcon
