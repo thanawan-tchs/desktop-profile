f262f10839845dbe17e7e74f0cd3fb073e9ef184
Thanawan <thanawan@Thanawans-MacBook-Air.local>
2026-08-16 01:40:56 +0700

    feat: change base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/desktop-profile/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
